d5bcba133da9bdaa9754aca0534926b984500b9e
zhangweixu <1689719986@qq.com>
2020-06-13 14:10:49 +0800

    装饰器

--- 函数.py
+++ 函数.py
@@ -31,4 +31,35 @@ if __name__ == '__main__':
     pd = Login.login_test("Cesare", "123456")
     print(Login.profile(pd))
 
+
 # print(profile(login_test("Cesare", "123456")))
+
+# 装饰器
+def login1(user, pawd):
+    def pro():
+        if user == "Cesare" and pawd == '123':
+            return "登录成功！"
+        else:
+            return "登录失败！"
+
+    return pro()
+
+
+print(login1("Cesare", "123"))
+
+
+def getInfo(func):
+    def inner():
+        print("Python接口自动化测试")
+        func()
+
+    return inner
+
+
+# 装饰器
+@getInfo
+def f1():
+    print("测试成功")
+
+
+f1()
