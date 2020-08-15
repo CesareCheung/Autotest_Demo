071f9c00920df8b4f29033d6e8e8d0556bfa4bc1
zhangweixu <1689719986@qq.com>
2020-08-15 20:18:02 +0800

    headers

--- requests库/header讲解.py
+++ requests库/header讲解.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author:序
+
+import requests
+
+data={
+	"sv": 200,
+	"tid": "gda",
+	"tv": "r20200810",
+	"st": "env"
+	      }
+
+header={"Content-Type":" application/json; charset=UTF-8"}
+url="https://pagead2.googlesyndication.com/getconfig/sodar"
+
+r=requests.get(url=url,params=data)
+print(r.text)
