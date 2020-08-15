ac0bd15b495581e3e589cca241d5fe273612f5c4
zhangweixu <1689719986@qq.com>
2020-08-15 21:46:28 +0800

    requests请求data与json参数

--- requests库/data与Json请求参数.py
+++ requests库/data与Json请求参数.py
@@ -9,3 +9,25 @@ application/json                  ---->使用data，也可使用json
 application/x-www-form-urlencoded ---->使用json,json格式的字符串数据类型
 
 """
+import requests
+
+
+data={
+	"sv": 200,
+	"tid": "gda",
+	"tv": "r20200810",
+	"st": "env"
+	      }
+
+header={"Content-Type":" application/json; charset=UTF-8"}
+url="https://pagead2.googlesyndication.com/getconfig/sodar"
+
+r=requests.get(url=url,json=data)
+print(r.text)
+
+"""
+使用json传参时必须满足的要求：
+1、Content-Type：application/json
+2、请求参数必须为字典
+但2必须在1的前提下才可以
+"""
